705dac11afd14f5bd10a8d99c40d8bc82d411fe5
fynui <99345843+fynui@users.noreply.github.com>
2022-10-31 10:30:21 +0800

    Update task.py

--- task.py
+++ task.py
@@ -8,15 +8,9 @@ class NodeClassification(nn.Module):
     def __init__(self, embedding_dim, label_num):
         super(NodeClassification, self).__init__()
         self.linear = nn.Linear(embedding_dim, label_num)
-        # self.linear1 = nn.Linear(embedding_dim, 16)
-        # self.linear2 = nn.Linear(16, label_num)
 
     def forward(self, nodes_embedding):
         x = self.linear(nodes_embedding)
-        # x = self.linear1(nodes_embedding)
-        # x = F.leaky_relu_(x)
-        # x = F.dropout(x, training=self.training)
-        # x = self.linear2(x)
         return torch.log_softmax(x, dim=-1)
 
     def evaluate(self, nodes_embedding, nodes_label):
